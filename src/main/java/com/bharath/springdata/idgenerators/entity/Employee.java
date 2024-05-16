b863415363eb9e6c6089419d2c041f656d184c22
naveen prasanth <naveenprasanth12@gmail.com>
2024-05-16 21:23:54 +0530

    emlpoyee changes

--- src/main/java/com/bharath/springdata/idgenerators/entity/Employee.java
+++ src/main/java/com/bharath/springdata/idgenerators/entity/Employee.java
@@ -1,8 +1,6 @@
 package com.bharath.springdata.idgenerators.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
@@ -11,10 +9,15 @@ import lombok.*;
 @Getter
 @Setter
 @ToString
+@Builder
 @Entity
 @Table(name = "employee")
 public class Employee {
     @Id
+    @TableGenerator(name = "employee_gen", table = "id_gen",
+            pkColumnName = "gen_name", valueColumnName = "gen_val",
+            allocationSize = 100)
+    @GeneratedValue(strategy = GenerationType.TABLE)
     private Long id;
     @NotNull
     private String name;
--- src/test/java/com/bharath/springdata/idgenerators/IdGeneratorsApplicationTests.java
+++ src/test/java/com/bharath/springdata/idgenerators/IdGeneratorsApplicationTests.java
@@ -1,13 +1,19 @@
 package com.bharath.springdata.idgenerators;
 
+import com.bharath.springdata.idgenerators.entity.Employee;
+import com.bharath.springdata.idgenerators.repos.EmployeeRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class IdGeneratorsApplicationTests {
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
     @Test
-    void contextLoads() {
+    void testCreateEmployee() {
+        employeeRepository.save(Employee.builder().name("naveen").build());
     }
 
 }
